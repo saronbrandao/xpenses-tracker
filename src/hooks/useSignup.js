c5ebb4ca355375af3d09f48b4adfd810052737fa
saronbrandao <saronbrandao@gmail.com>
2022-10-20 18:57:41 +0200

    master

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react';
 import { projectAuth } from '../firebase/config';
 import { useAuthContext } from '../hooks/useAuthContext';
+import { useHistory } from 'react-router-dom';
 
 export const useSignup = () => {
   const [isCancelled, setIsCancelled] = useState(false);
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext();
+  const history = useHistory();
 
   const signup = async (email, password, displayName) => {
     setError(null);
@@ -32,6 +34,7 @@ export const useSignup = () => {
       if (!isCancelled) {
         setIsPending(false);
         setError(null);
+        history.push('/');
       }
     } catch (err) {
       //catch error
