8944f5c359df42fa6229da0454a082bec9a1d4b6
saronbrandao <saronbrandao@gmail.com>
2022-10-23 23:42:36 +0200

    version1

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,22 @@
+import { useAuthContext } from '../../hooks/useAuthContext';
+import { useCollection } from '../../hooks/useCollection';
+import { TransactionList } from './TransactionList';
+
+import TransactionForm from './TransactionForm';
 import styles from './Home.module.css';
-import TransactionForm from './transactionForm';
 
 const Home = () => {
+  const { user } = useAuthContext();
+  const { documents, error } = useCollection('transactions');
+
   return (
     <div className={styles.container}>
-      <div className={styles.content}>Transaction list</div>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionsList transactions={documents} />}
+      </div>
       <div className={styles.sidebar}>
-        <TransactionForm />
+        <TransactionForm uid={user.uid} />
       </div>
     </div>
   );
---
+++ src/pages/home/TransactionList.js
@@ -0,0 +1,5 @@
+import style from './Home.module.css';
+
+export const TransactionList = () => {
+  return <div></div>;
+};
