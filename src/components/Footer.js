648dd4da8f15bde05b29d3027114552936a4dc6e
Saron <saronbrandao@gmail.com>
2022-12-23 04:40:27 +0100

    corrections

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,17 @@ import styles from './Footer.module.scss';
 
 const Footer = () => {
   return (
-    <div className={styles.footer}>
-      <div className={styles.container__text}>
-        <p>@2022 Berlin - Germany</p>
-        <p>Developed by Saron Brandao Web Dev</p>
-      </div>
-      <div className={styles.container__logos}>
-        <a href="https://www.linkedin.com/in/saronbrandao/" target="blank">
-          <i className="fa-brands fa-linkedin"></i>
-        </a>
+    <div className={styles.footer_container}>
+      <div className={styles.footer}>
+        <div className={styles.container__text}>
+          <p>@2022 Berlin - Germany</p>
+          <p>Developed by Saron Brandao Web Dev</p>
+        </div>
+        <div className={styles.container__logos}>
+          <a href="https://www.linkedin.com/in/saronbrandao/" target="blank">
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
+        </div>
       </div>
     </div>
   );
