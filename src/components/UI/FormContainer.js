5c1eab52ecba9112da7717acf6e30e912eaca94b
Saron <saronbrandao@gmail.com>
2022-12-23 04:17:31 +0100

    corrections

--- src/components/UI/FormContainer.js
+++ src/components/UI/FormContainer.js
@@ -1,7 +1,11 @@
 import style from './FormContainer.module.scss';
 
 const FormContainer = (props) => {
-  return <div className={style.container}>{props.children}</div>;
+  return (
+    <div className={style.main_container}>
+      <div className={style.container}>{props.children}</div>
+    </div>
+  );
 };
 
 export default FormContainer;
